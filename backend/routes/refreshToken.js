c0316cbe97f6adec5c7a56c9054c1b058132d5d9
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-10-19 23:12:55 -0300

    feat:verifytoken file is created to check the accestoken and refreshtoken in nodejs

--- backend/routes/refreshToken.js
+++ backend/routes/refreshToken.js
@@ -1,6 +1,34 @@
+const getTokenFromHeader = require("../auth/getTokenFromHeader");
+const { verifyRefreshToken } = require("../auth/verifyToken");
+const Token = require("../schema/token");
+
 const router = require("express").Router();
 
-router.get("/", (req,res) => {
+router.post("/", async (req,res) => {
+
+    const refreshtoken = getTokenFromHeader(req.headers);
+
+    if(refreshtoken){
+      try {
+        const found = await Token.findOne({token: refreshToken})
+        if(!found){
+           return res.status(401).send(jsonResponse(401, {error: "Unauthorized"}));
+        }
+
+        const payload = verifyRefreshToken(found.token);
+        if(payload){
+            const accessToken = generateAccessToken(payload);
+
+            return res.status(200).json(jsonResponse(200, {accessToken}));
+        } else {
+            return res.status(401).send(jsonResponse(401, {error: "Unauthorized"}));
+        }
+      } catch (error) {
+        return res.status(401).send(jsonResponse(401, {error: "Unauthorized"}));
+      }  
+    } else {
+        res.status(401).send(jsonResponse(401, {error: "Unauthorized"}));
+    }
     res.send("refreshtoken");
 });
 
